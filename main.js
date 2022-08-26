65a508528569f32af212e224235b3a402176cdbf
Sugut <sugutvictorine@gmail.com>
2022-08-26 17:53:27 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// someElement.addEventListener(
+//   "mouseup",
+//   handleMouseUp,
+//   passiveSupported ? { passive: true } : false
+// );
+document.querySelectorAll("like-glyph").addEventListener("click",myFunction)
 
+let myFunction=(e)=>{
+  let heart = e.target;
+}
+mimicServerCall()
+.then((response)=> {
+                   if(response.ok){
+                   heart.innerText=FULL_HEART[heart.innerText]
+                   heart.className="activated-heart"
+                  }
+                  else {
+                    heart.innerText=EMPTY_HEART[heart.innerText]
+                    heart.className=" "
+                 }
+})
 
+.catch((error)=> {
+     const errorModal=document.getElementById("modal")
+     errorModal.textContent= error;
+     setTimeout(() => errorModal.className ="hidden", 3000)
+});
+
+ 
 
 
 //------------------------------------------------------------------------------
